a6911c4349005547ddc5ecc9d6338d88c97869ca
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-19 18:13:13 +0530

    Added: Welcome message

---
+++ src/compare/line/LineComparison.java
@@ -0,0 +1,9 @@
+package compare.line;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program!");
+	}
+}
